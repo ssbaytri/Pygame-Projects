790c1e01f3ebad894df7113d70fdd368e98a158c
ssbaytri <azjoker333@gmail.com>
2024-11-10 23:11:40 +0100

    draw_screen()

--- Typing Racer Game/game.py
+++ Typing Racer Game/game.py
@@ -10,15 +10,35 @@ surface = pygame.Surface((WIDTH, HEIGHT), pygame.SRCALPHA)
 timer = pygame.time.Clock()
 FPS = 60
 
+# Game vars
+level = 0
+active_str = "test string"
+score = 0
+high_score = 1
+lives = 5
+
 # Assets loading
+header_font = pygame.font.Font("assets/fonts/Square.ttf", 50)
+pause_font = pygame.font.Font("assets/fonts/1up.ttf", 38)
+banner_font = pygame.font.Font("assets/fonts/1up.ttf", 28)
+font = pygame.font.Font("assets/fonts/AldotheApache.ttf", 48)
 
 
 def draw_screen():
+    # draw lines for background and titlebar
     pygame.draw.rect(screen, (32, 42, 68), [0, HEIGHT - 100, WIDTH, 100])
     pygame.draw.rect(screen, "white", [0, 0, WIDTH, HEIGHT], 5)
     pygame.draw.line(screen, "white", (250, HEIGHT - 100), (250, HEIGHT), 2)
     pygame.draw.line(screen, "white", (700, HEIGHT - 100), (700, HEIGHT), 2)
     pygame.draw.line(screen, "white", (0, HEIGHT - 100), (WIDTH, HEIGHT - 100), 2)
+    pygame.draw.rect(screen, "black", [0, 0, WIDTH, HEIGHT], 2)
+
+    # drawing texts
+    screen.blit(header_font.render(f"Level: {level}", True, "white"), (10, HEIGHT - 75))
+    screen.blit(header_font.render(f'"{active_str}"', True, "white"), (270, HEIGHT - 75))
+    screen.blit(banner_font.render(f"Score: {score}", True, "black"), (250, 10))
+    screen.blit(banner_font.render(f"Best: {high_score}", True, "black"), (550, 10))
+    screen.blit(banner_font.render(f"Lives: {lives}", True, "black"), (10, 10))
 
 
 running = True
