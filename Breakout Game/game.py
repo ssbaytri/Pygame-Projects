da304b7f5f7a6551c10d612fc109138e35ffa492
ssbaytri <azjoker333@gmail.com>
2025-03-05 05:03:48 +0000

    window

--- Breakout Game/game.py
+++ Breakout Game/game.py
@@ -1,1 +1,17 @@
-# game start
+import pygame
+from pygame.locals import *
+
+pygame.init()
+
+window_width, window_height = 600, 600
+
+window = pygame.display.set_mode((window_width, window_height))
+pygame.display.set_caption("Breakout")
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
+            running = False
+
+pygame.quit()
