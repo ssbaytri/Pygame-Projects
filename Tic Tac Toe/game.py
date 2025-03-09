310b62f53e3b24995c1638e7f5eb20b4572f417e
ssbaytri <azjoker333@gmail.com>
2025-03-09 03:06:53 +0000

    window setup

--- Tic Tac Toe/game.py
+++ Tic Tac Toe/game.py
@@ -1,1 +1,14 @@
-# Game Start
+import pygame
+from pygame.locals import *
+
+window_width, window_height = 300, 300
+window = pygame.display.set_mode((window_width, window_height))
+pygame.display.set_caption("Tic Tac Toe")
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
+            running = False
+
+pygame.quit()
