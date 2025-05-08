839e0047096c637fab38fd224abe0ee963e9845f
ssbaytri <azjoker333@gmail.com>
2025-05-08 01:46:53 +0100

    display setup

---
+++ RayCasting/main.py
@@ -0,0 +1,10 @@
+import pygame
+from settings import *
+
+screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
+            pygame.quit()
+            exit()
