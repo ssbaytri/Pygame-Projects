874c8a0f6b009274a3c536f05b67c24a620f95c2
ssbaytri <azjoker333@gmail.com>
2025-05-08 02:02:02 +0100

    draw the map

--- RayCasting/main.py
+++ RayCasting/main.py
@@ -1,10 +1,17 @@
 import pygame
 from settings import *
+from map import Map
 
 screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+game_map = Map()
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
             pygame.quit()
             exit()
+
+    screen.fill("black")
+    game_map.render(screen)
+
+    pygame.display.update()
---
+++ RayCasting/map.py
@@ -0,0 +1,27 @@
+import pygame
+from settings import *
+
+class Map:
+    def __init__(self):
+        self.grid = [
+            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+            [1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1],
+            [1, 0, 0, 1, 0, 0, 0, 1, 1, 1, 0, 1, 0, 0, 1],
+            [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+            [1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1],
+            [1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+            [1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1],
+            [1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+            [1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+        ]
+
+    def render(self, screen):
+        for y in range(ROWS):
+            for x in range(COLS):
+                tile_x = x * TILE_SIZE
+                tile_y = y * TILE_SIZE
+                if self.grid[y][x] == 0:
+                    pygame.draw.rect(screen, (255, 255, 255), (tile_x, tile_y, TILE_SIZE - 1, TILE_SIZE - 1))
+                if self.grid[y][x] == 1:
+                    pygame.draw.rect(screen, (40, 40, 40), (tile_x, tile_y, TILE_SIZE - 1, TILE_SIZE - 1))
