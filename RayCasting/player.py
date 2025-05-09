53fe5dbe070eb9867f464ba49073627dd4182b1f
ssbaytri <azjoker333@gmail.com>
2025-05-09 02:11:29 +0100

    player dir

--- RayCasting/player.py
+++ RayCasting/player.py
@@ -7,7 +7,11 @@ class Player:
     def __init__(self):
         self.x = WINDOW_WIDTH / 2
         self.y = WINDOW_HEIGHT / 2
-        self.radius = 6
+        self.radius = 3
+        self.rot_angle = math.pi / 4
 
     def render(self, screen):
         pygame.draw.circle(screen, "red", (self.x, self.y), self.radius)
+
+        pygame.draw.line(screen, "red", (self.x, self.y),
+                         (self.x + math.cos(self.rot_angle) * 50, self.y + math.sin(self.rot_angle) * 50))
