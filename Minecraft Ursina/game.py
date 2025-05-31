b5234ffb8caf486b0ea3580f28f56bb24bfae0ec
ssbaytri <azjoker333@gmail.com>
2025-05-31 12:43:09 +0100

    TBC

--- Minecraft Ursina/game.py
+++ Minecraft Ursina/game.py
@@ -1,37 +1,23 @@
 from ursina import *
+from ursina.prefabs.first_person_controller import FirstPersonController
 
-class Cube(Entity):
-	def __init__(self):
-		super().__init__(
-			model='cube',
-			color=color.white,
-			texture='white_cube',
-			rotation=Vec3(45, 45, 45),
-			position=(4, 3)
-		)
-
-class Button(Button):
-	def __init__(self):
+class Voxel(Button):
+	def __init__(self, position=(0, 0, 0)):
 		super().__init__(
 			parent=scene,
+			position=position,
 			model='cube',
-			texture='brick',
-			color=color.red,
-			highlight_color=color.blue,
-			pressed_color=color.green
+			origin_y=0.5,
+			texture='white_cube',
+			color=color.white,
+			highlight_color=color.lime
 		)
 
-	def input(self, key):
-		if self.hovered:
-			if key == "left mouse down":
-				print("james say what")
-
-def input(key):
-	if key == 'escape':
-		quit()
 
 app = Ursina()
 
-test_cube = Cube()
-test_btn = Button()
+for z in range(10):
+	for x in range(10):
+		voxel = Voxel(position=(z, 0, x))
+
 app.run()
