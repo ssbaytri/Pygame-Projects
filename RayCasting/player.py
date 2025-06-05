3a106240f000172e25b86135825f8ffee231c856
ssbaytri <azjoker333@gmail.com>
2025-06-05 01:45:41 +0100

    revision

--- RayCasting/player.py
+++ RayCasting/player.py
@@ -8,34 +8,35 @@ class Player:
         self.x = WINDOW_WIDTH / 2
         self.y = WINDOW_HEIGHT / 2
         self.radius = 3
-        self.turn_dir = 0
-        self.walk_dir = 0
-        self.rot_angle = math.pi / 4
-        self.mov_speed = 1
-        self.rot_speed = 1.5 * (math.pi / 180)
+        self.turnDir = 0
+        self.walkDir = 0
+        self.rot_angle = 90 * (math.pi / 180)
+        self.move_speed = 1.5
+        self.rot_speed = 2 * (math.pi / 180)
 
     def update(self):
         keys = pygame.key.get_pressed()
 
-        self.turn_dir = 0
-        self.walk_dir = 0
+        self.turnDir = 0
+        self.walkDir = 0
 
         if keys[pygame.K_RIGHT]:
-            self.turn_dir = 1
+            self.turnDir = 1
         if keys[pygame.K_LEFT]:
-            self.turn_dir = -1
+            self.turnDir = -1
         if keys[pygame.K_UP]:
-            self.walk_dir = 1
+            self.walkDir = 1
         if keys[pygame.K_DOWN]:
-            self.walk_dir = -1
+            self.walkDir = -1
 
-        move_step = self.walk_dir * self.mov_speed
-        self.rot_angle += self.turn_dir * self.rot_speed
+        self.rot_angle += self.turnDir * self.rot_speed
+        self.rot_angle %= 2 * math.pi
+
+        move_step = self.walkDir * self.move_speed
         self.x += math.cos(self.rot_angle) * move_step
         self.y += math.sin(self.rot_angle) * move_step
 
     def render(self, screen):
         pygame.draw.circle(screen, "red", (self.x, self.y), self.radius)
-
         pygame.draw.line(screen, "red", (self.x, self.y),
                          (self.x + math.cos(self.rot_angle) * 50, self.y + math.sin(self.rot_angle) * 50))
