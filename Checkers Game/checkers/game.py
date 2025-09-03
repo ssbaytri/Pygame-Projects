0d493f0f492a1d56b15f08c93d016fdf984d0586
ssbaytri <azjoker333@gmail.com>
2025-09-03 10:24:04 +0100

    select()

--- Checkers Game/checkers/game.py
+++ Checkers Game/checkers/game.py
@@ -4,10 +4,7 @@ from checkers.board import Board
 
 class Game:
     def __init__(self, win):
-        self.selected = None
-        self.board = Board()
-        self.turn = P1_COLOR
-        self.valid_moves = {}
+        self._init()
         self.win = win
 
     def update(self):
@@ -15,8 +12,41 @@ class Game:
         self.board.draw(self.win)
         pygame.display.update()
 
-    def reset(self):
+    def _init(self):
         self.selected = None
         self.board = Board()
         self.turn = P1_COLOR
         self.valid_moves = {}
+
+    def reset(self):
+        self._init()
+
+    def select(self, row, col):
+        if self.selected:
+            result = self._move(row, col)
+            if not result:
+                self.selected = None
+                self.select(row, col)
+
+        else:
+            piece = self.board.get_piece(row, col)
+            if piece != 0 and piece.color == self.turn:
+                self.selected = piece
+                self.valid_moves = self.board.get_valid_moves(piece)
+                return True
+        return False
+
+    def _move(self, row, col):
+        piece = self.board.get_piece(row, col)
+        if self.selected and piece == 0 and (row, col) in self.valid_moves:
+            self.board.move(self.selected, row, col)
+            self.change_turn()
+        else:
+            return False
+        return True
+
+    def change_turn(self):
+        if self.turn == P1_COLOR:
+            self.turn = P2_COLOR
+        else:
+            self.turn = P1_COLOR
