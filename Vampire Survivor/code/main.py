09dc2dffee494db3b500e4336451ec40c8416424
ssbaytri <azjoker333@gmail.com>
2025-10-25 08:06:11 +0100

    display setup

--- Vampire Survivor/code/main.py
+++ Vampire Survivor/code/main.py
@@ -1,1 +1,26 @@
 from settings import *
+
+
+class Game:
+    def __init__(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+        pygame.display.set_caption("Vampire Survivor")
+        self.clock = pygame.time.Clock()
+        self.running = True
+
+    def run(self):
+        dt = self.clock.tick() / 1000
+
+        while self.running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
+                    self.running = False
+
+            pygame.display.update()
+        pygame.quit()
+
+
+if __name__ == "__main__":
+    game = Game()
+    game.run()
