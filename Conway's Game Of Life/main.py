e8c5b210e556080ca973751b45beebf75f8542d0
ssbaytri <azjoker333@gmail.com>
2025-11-25 02:34:42 +0100

    draw_grid

--- Conway's Game Of Life/main.py
+++ Conway's Game Of Life/main.py
@@ -11,9 +11,17 @@ window = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Conway's Game Of Life")
 clock = pygame.time.Clock()
 
+def draw_grid(pos):
+    for row in range(GRID_HEIGHT):
+        pygame.draw.line(window, "black", (0, row * TILE_SIZE), (WIDTH, row * TILE_SIZE))
+        
+    for col in range(GRID_WIDTH):
+        pygame.draw.line(window, "black", (col * TILE_SIZE, 0), (col * TILE_SIZE, HEIGHT))
+
 def main():
     running = True
     
+    positions = set()
     while running:
         clock.tick(FPS)
         
@@ -21,7 +29,11 @@ def main():
             if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
                 running = False
                 
-    pygame.quit()
+        window.fill("grey")
+        draw_grid(positions)
+        pygame.display.update()
     
+    pygame.quit()
+
 if __name__ == "__main__":
 	main()
