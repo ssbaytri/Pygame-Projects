5ac4888aaa58dafc44e23ec5f1d56a714b161293
ssbaytri <azjoker333@gmail.com>
2025-12-23 21:57:54 +0100

    Objects and Ground Tiles

--- Vampire Survivor/code/main.py
+++ Vampire Survivor/code/main.py
@@ -2,6 +2,7 @@ from settings import *
 from player import Player
 from sprites import *
 from random import randint
+from pytmx.util_pygame import load_pygame
 
 
 class Game:
@@ -16,12 +17,19 @@ class Game:
         self.all_sprites = pygame.sprite.Group()
         self.collision_sprites = pygame.sprite.Group()
 
+        self.setup()
+
         # sprites
         self.player = Player((400, 300), self.all_sprites, self.collision_sprites)
-        for i in range(6):
-            x, y = randint(0, WINDOW_WIDTH), randint(0, WINDOW_HEIGHT)
-            w, h = randint(60, 100), randint(50, 100)
-            CollisionSprite((x, y), (w, h), (self.all_sprites, self.collision_sprites))
+        
+    def setup(self):
+        map = load_pygame(join("../data", "maps", "world.tmx"))
+            
+        for x, y, image in map.get_layer_by_name("Ground").tiles():
+            Sprite((x * TILE_SIZE, y * TILE_SIZE), image, self.all_sprites)
+            
+        for obj in map.get_layer_by_name("Objects"):
+            CollisionSprite((obj.x, obj.y), obj.image, (self.all_sprites, self.collision_sprites))
 
     def run(self):
         while self.running:
--- Vampire Survivor/code/sprites.py
+++ Vampire Survivor/code/sprites.py
@@ -1,10 +1,16 @@
 from settings import *
 
 
-class CollisionSprite(pygame.sprite.Sprite):
-    def __init__(self, pos, size, groups):
+class Sprite(pygame.sprite.Sprite):
+    def __init__(self, pos, surf, groups):
         super().__init__(groups)
-        self.image = pygame.Surface(size)
-        self.image.fill("blue")
-        self.rect = self.image.get_rect(center=pos)
+        self.image = surf
+        self.rect = self.image.get_rect(topleft=pos)
+
+
+class CollisionSprite(pygame.sprite.Sprite):
+    def __init__(self, pos, surf, groups):
+        super().__init__(groups)
+        self.image = surf
+        self.rect = self.image.get_rect(topleft=pos)
     
