9080cb65671e22459dd0517fde19e9d5a92480c6
$ubZ3r0 <106231563+ssbaytri@users.noreply.github.com>
2025-12-29 23:42:25 +0100

    TBC

--- Vampire Survivor/code/sprites.py
+++ Vampire Survivor/code/sprites.py
@@ -14,4 +14,15 @@ class CollisionSprite(pygame.sprite.Sprite):
         super().__init__(groups)
         self.image = surf
         self.rect = self.image.get_rect(topleft=pos)
+        
+
+class Gun(pygame.sprite.Sprite):
+    def __inti__(self, player, groups):
+        self.player = player
+        self.dist = 140
+        self.player_dir = pygame.Vector2(1, 0)
+        
+        super().__init__(groups)
+        self.gun_surf = pygame.image.load(join("../images", "gun", "gun.png")).convert_alpha()
+        self.image = self.gun_surf
     
