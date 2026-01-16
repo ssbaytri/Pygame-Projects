8e25d2399b2477330e95cce3052e44d2464ee683
ssbaytri <azjoker333@gmail.com>
2026-01-16 02:28:04 +0100

    move enemies

--- Vampire Survivor/code/sprites.py
+++ Vampire Survivor/code/sprites.py
@@ -87,7 +87,14 @@ class Enemy(pygame.sprite.Sprite):
         self.frame_idx += self.animation_speed * dt
         self.image = self.frames[int(self.frame_idx) % len(self.frames)]
         
+    def move(self, dt):
+        player_pos = pygame.Vector2(self.player.rect.center)
+        enemy_pos = pygame.Vector2(self.rect.center)
+        self.dir = (player_pos - enemy_pos).normalize()
+        
+        self.rect.center += self.dir * self.speed * dt
+        
     def update(self, dt):
-        # self.move(dt)
+        self.move(dt)
         self.animate(dt)
         
