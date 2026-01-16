691edf8dc8b8747333ad34226c2e12b70081eeb4
ssbaytri <azjoker333@gmail.com>
2026-01-17 00:01:22 +0100

    enemy collision

--- Vampire Survivor/code/sprites.py
+++ Vampire Survivor/code/sprites.py
@@ -80,7 +80,7 @@ class Enemy(pygame.sprite.Sprite):
         self.rect = self.image.get_rect(center=pos)
         self.hitbox_rect = self.rect.inflate(-20, -40)
         self.collision_sprites = collision_sprites
-        self.dir = pygame.Vector2()
+        self.direction = pygame.Vector2()
         self.speed = 350
         
     def animate(self, dt):
@@ -90,9 +90,23 @@ class Enemy(pygame.sprite.Sprite):
     def move(self, dt):
         player_pos = pygame.Vector2(self.player.rect.center)
         enemy_pos = pygame.Vector2(self.rect.center)
-        self.dir = (player_pos - enemy_pos).normalize()
+        self.direction = (player_pos - enemy_pos).normalize()
         
-        self.rect.center += self.dir * self.speed * dt
+        self.hitbox_rect.x += self.direction.x * self.speed * dt
+        self.collision("horizontal")
+        self.hitbox_rect.y += self.direction.y * self.speed * dt
+        self.collision("vertical")
+        self.rect.center = self.hitbox_rect.center
+        
+    def collision(self, direction):
+        for sprite in self.collision_sprites:
+            if sprite.rect.colliderect(self.hitbox_rect):
+                if direction == "horizontal":
+                    if self.direction.x > 0 : self.hitbox_rect.right = sprite.rect.left
+                    if self.direction.x < 0 : self.hitbox_rect.left = sprite.rect.right
+                else:
+                    if self.direction.y < 0 : self.hitbox_rect.top = sprite.rect.bottom
+                    if self.direction.y > 0 : self.hitbox_rect.bottom = sprite.rect.top
         
     def update(self, dt):
         self.move(dt)
