f243c3c94c60d58aa40dd5065a03a28ac21b07b0
ssbaytri <azjoker333@gmail.com>
2026-01-18 17:46:25 +0100

    player movement

--- Platform/code/sprites.py
+++ Platform/code/sprites.py
@@ -6,3 +6,31 @@ class Sprite(pygame.sprite.Sprite):
         super().__init__(groups)
         self.image = surf
         self.rect = self.image.get_rect(topleft=pos)
+
+
+class Player(Sprite):
+    def __init__(self, pos, groups, collision_sprites):
+        surf = pygame.Surface((40, 80))
+        super().__init__(pos, surf, groups)
+        
+        # movement & collisions
+        self.direction = pygame.Vector2()
+        self.collision_sprites = collision_sprites
+        self.speed = 400
+        
+    def input(self):
+        keys = pygame.key.get_pressed()
+        self.direction.x = int(keys[pygame.K_RIGHT]) - int(keys[pygame.K_LEFT])
+        self.direction.y = int(keys[pygame.K_DOWN]) - int(keys[pygame.K_UP])
+        self.direction = self.direction.normalize() if self.direction else self.direction
+        
+    def collision(self, direction):
+        pass
+        
+    def move(self, dt):
+        self.rect.x += self.direction.x * self.speed * dt
+        self.rect.y += self.direction.y * self.speed * dt
+        
+    def update(self, dt):
+        self.input()
+        self.move(dt)
